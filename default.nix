86500c827d786c1a6bd2e8ec20b7de18b373fdc5
Daniël de Kok <me@danieldk.eu>
2020-02-07 15:37:35 +0100

    pytorch.v1_4_1: init at f56c59ead

--- default.nix
+++ default.nix
@@ -1,8 +1,20 @@
 let
   sources = import ./nix/sources.nix;
-  nixpkgs-unstable = import sources.nixpkgs-unstable {};
+  nixpkgs = import sources.nixpkgs {};
   rocm = let
-    self = (import sources.rocm) (nixpkgs-unstable // self) nixpkgs-unstable;
+    self = (import sources.nixos-rocm) (nixpkgs // self) nixpkgs;
     in self;
-in {
+in with nixpkgs; {
+  pytorch = {
+    v1_4_0 = python37Packages.callPackage pkgs/pytorch/v1_4_0 {
+      inherit (rocm) rocr miopengemm rocsparse hipsparse rocthrust
+        rccl rocrand rocblas rocfft rocprim hipcub roctracer rocm-cmake;
+
+      miopen = rocm.miopen-hip;
+      hip = rocm.hip;
+      comgr = rocm.hcc-comgr;
+      openmp = rocm.hcc-openmp;
+      hcc = rocm.hcc-unwrapped;
+    };
+  };
 }
