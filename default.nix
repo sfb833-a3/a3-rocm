b102e60acf386771a1f68bb02e1ef937f6364d25
Daniël de Kok <me@danieldk.eu>
2020-02-09 13:23:40 +0100

    Fix race condition in hipEventRecord

--- default.nix
+++ default.nix
@@ -6,7 +6,19 @@ let
   sources = import ./nix/sources.nix;
   rocm = let
     self = (import sources.nixos-rocm) (nixpkgs // self) nixpkgs;
-    in self;
+  in self // {
+    hip = self.hip.overrideAttrs (attrs: rec {
+      patches = attrs.patches or [] ++ [
+        # Fixes race condition in hipEventRecord, remove with ROCM 3.0.
+        # https://github.com/ROCm-Developer-Tools/HIP/pull/1620
+        (nixpkgs.fetchpatch {
+          name = "fix-hipEventRecord-race.patch";
+          url = "https://patch-diff.githubusercontent.com/raw/ROCm-Developer-Tools/HIP/pull/1620.patch";
+          sha256 = "1v81ss1ls6jyg2mfspz6zq8a4ajkz5qfhgfwvqrsaih9rbjy8n76";
+        })
+      ];
+    });
+  };
 in with nixpkgs; {
   libtensorflow = callPackage pkgs/libtensorflow {
     inherit (rocm) hcc hcc-unwrapped hip miopen-hip miopengemm
